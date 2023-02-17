b70c4c574294c7ff80c3ea1afd0d72413183f8a2
Chad Bartels <chad.bartels@gmail.com>
2023-02-17 10:45:47 -0500

    Update styles and added scripts for header

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,19 @@
 const updateHeader = () => {
-  const header = document.querySelector('header');
-  
+  const header = document.querySelector('header')
+  const hero = document.querySelector('.hero')
+  const logo = document.querySelector('.logo img')
+
+  document.addEventListener('scroll', (e) => {
+    if (hero.getBoundingClientRect().top < -100) {
+      header.classList.add('scrolled')
+      logo.attributes.src.value = '/static/images/UC_mark_white.svg'
+      logo.style.width = '40px'
+    } else {
+      header.classList.remove('scrolled')
+      logo.attributes.src.value = '/static/images/UC_logo_white.svg'
+      logo.style.width = '200px'
+    }
+  })
 }
+
+updateHeader()
