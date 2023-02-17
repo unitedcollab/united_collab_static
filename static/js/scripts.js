8c8e95e92a66c51c0d0847d387e006a7d0f1f16b
Chad Bartels <chad.bartels@gmail.com>
2023-02-17 11:38:42 -0500

    Update scripts to stop continuous logo replacement on scroll

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,15 +3,20 @@ const updateHeader = () => {
   const hero = document.querySelector('.hero')
   const logo = document.querySelector('.logo img')
 
+  var fired = false
+
   document.addEventListener('scroll', (e) => {
-    if (hero.getBoundingClientRect().top < -100) {
+    if (hero.getBoundingClientRect().top < -100 && fired === false) {
       header.classList.add('scrolled')
       logo.attributes.src.value = '/static/images/UC_mark_white.svg'
       logo.style.width = '40px'
-    } else {
+      fired = true
+    }
+    if (hero.getBoundingClientRect().top > -100 && fired === true) {
       header.classList.remove('scrolled')
       logo.attributes.src.value = '/static/images/UC_logo_white.svg'
       logo.style.width = '200px'
+      fired = false
     }
   })
 }
